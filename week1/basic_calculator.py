cd4098a05da16d2b9dcfab0cb730d9c04c4f1bb6
emma-obelenge <obeleemmanuel@gmail.com>
2025-03-14 09:31:28 +0100

    week 1 assignment complete

---
+++ week1/basic_calculator.py
@@ -0,0 +1,19 @@
+operators = ["+", "-", "x", "/"]
+
+print("Welcome to the Simple Calculator Program")
+print("This program only calculates for")
+print("addition (+), subtraction (-), multiplication (x), and division (/)")
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operator = input("what are you calculating for? (+, -, x, or /): ")
+#parsing the user operator input
+if(operator == "+"):
+    print(str(num1) + " + " + str(num2) + " = " + str(num1 + num2))
+elif(operator == "-"):
+    print(str(num1) + " - " + str(num2) + " = " + str(round((num1 - num2), 2)))
+elif(operator == "x" or operator == "*"):
+    print(str(num1) + " x " + str(num2) + " = " + str(round((num1 * num2), 2)))
+elif(operator == "/"):
+    print(str(num1) + " / " + str(num2) + " = " + str(round((num1 / num2), 2)))
+else:
+    print("invalid operator" + " " + str(num1) + " " + operator + " " + str(num2))
